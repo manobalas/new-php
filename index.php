cc004bce0539de53f52c5bf2741c70949b681067
Manobala S <manobala.s@hotmail.com>
2020-07-24 16:36:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <body>
         
         <p id="change">Checking storage</p>
+        <a href="/index2.php?option=com_jumi&amp;fileid=3&amp;Itemid=11"
+   onclick="window.open(this.href,'targetWindow',
+                                   `toolbar=no,
+                                    location=no,
+                                    status=no,
+                                    menubar=no,
+                                    scrollbars=yes,
+                                    resizable=yes,
+                                    width=SomeSize,
+                                    height=SomeSize`);
+ return false;">Popup link</a>
         <script>
             setInterval(function(){ 
                 // check

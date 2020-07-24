57b4e16b832ea85986e32aa485b79d7d9b5df8e6
Manobala S <manobala.s@hotmail.com>
2020-07-24 16:34:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
 
-echo "Hello world";
+<html>
+    <head>
+        
+    </head>
+    <body>
+        
+        <p id="change">Checking storage</p>
+        <script>
+            setInterval(function(){ 
+                // check
+                if(localStorage.getItem("b_token") != null) {
+                    document.getElementById("change").innerHTML = "Cookie Available"
+                }
+            }, 1000);
+        </script>
+    </body>
+</html>

d925b621f542a19de9c6d6526feab8d7647c40a5
Manobala S <manobala.s@hotmail.com>
2020-07-24 16:46:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,23 +6,22 @@
     <body>
         
         <p id="change">Checking storage</p>
-        <a href="auth/index.php?clientId=ranjithdinakaran&clientSecret=Drk_13232104&prefix_url=d-lab-2019"
-   onclick="window.open(this.href,'targetWindow',
-                                   `toolbar=no,
-                                    location=no,
-                                    status=no,
-                                    menubar=no,
-                                    scrollbars=yes,
-                                    resizable=yes,
-                                    width=500,
-                                    height=500`);
- return false;">Popup link</a>
+        <a href="auth/index.php?clientId=ranjithdinakaran&clientSecret=Drk_13232104&prefix_url=d-lab-2019" onclick="openWin(this.href); return false;">Popup link</a>
         <script>
+            var myWindow;
+            function openWin(link) {
+                myWindow = window.open(link,'targetWindow',`toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=500,height=500`);
+            }
+            
+            function closeWin() {
+              myWindow.close();
+            }
+            
             setInterval(function(){ 
                 // check
                 if(localStorage.getItem("b_token") != null) {
-                    document.getElementById("change").innerHTML = "Cookie Available"
-                    window.close()
+                    document.getElementById("change").innerHTML = "Cookie Available";
+                    closeWin();
                 }
             }, 1000);
         </script>
